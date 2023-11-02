16a6ca15aea79a28d7649f7218f6d0eab637c0cb
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-11-02 15:37:49 +0200

    file cleanup

--- leader/globals/globals.go
+++
@@ -1,9 +0,0 @@
-package globals
-
-var NetworkName = "lazarbase-network"
-
-var ProjectPath = "./data/projects/"
-
-var VMPath = "/home/lazarbase/data"
-
-var DashboardImage = "monsieurlazar/lazarbase-dashboard"
